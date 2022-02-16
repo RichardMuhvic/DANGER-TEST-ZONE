b9cbe2fd26331e4aeaf548c200fa1ee8d9fa00ca
Richard Muhvic <richardmuhvic@gmail.com>
2022-02-16 12:33:09 +0100

    ajustement de certaines petites erreurs

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function parallax(e) {
 
         const position = move.getAttribute('data-value');
 
-        const x = (window.innerWidth - e.pageX * position) / 100;
-        const y = (window.innerHeight - e.pageY * position) / 100;
+        const x = (window.innerWidth - e.pageX * position) / 150;
+        const y = (window.innerHeight - e.pageY * position) / 150;
 
         move.style.transform = `translateX(${x}px) translateY(${y}px)`;
     });
